84e9b9a1adcea803746f147f5eadb3bedda6a1ed
ItsRifter <d_thomas_smith30@hotmail.com>
2024-10-02 11:48:23 +0100

    Check for multiple owners + more game logic

--- Assets/YOUR_STUFF_HERE/Scripts/ChoiceSlot.cs
+++ Assets/YOUR_STUFF_HERE/Scripts/ChoiceSlot.cs
@@ -12,22 +12,30 @@ public class ChoiceSlot : MonoBehaviour
 
     public Color HoverColour = Color.grey;
 
+    public bool IsSelected;
+    public int PlayerOwner;
+
     Color curColor;
     Image slotIcon;
+
+    public int RowIndex;
 
     void Start()
     {
         slotIcon = GetComponent<Image>();
+
+        IsSelected = false;
+        PlayerOwner = -1;
     }
 
-    Color GetSelectingPlayerColour(int ply)
+    public Color GetSelectingPlayerColour(int ply)
     {
         switch(ply)
         {
             case 0: return PlyOneColour;
             case 1: return PlyTwoColour;
             case 2: return PlyThreeColour;
-            case 3: return HoverColour;
+            case 3: return PlyFourColour;
 
             default: return Color.white;
         }
@@ -40,10 +48,15 @@ public class ChoiceSlot : MonoBehaviour
         slotIcon.color = plyCol;
     }
 
-    public void OnLockedIn(int player)
+    public void Assign(int player)
     {
-
+        IsSelected = true;
+        PlayerOwner = player;
     }
 
-
+    public void Reset()
+    {
+        IsSelected = false;
+        PlayerOwner = -1;
+    }
 }
--- Assets/YOUR_STUFF_HERE/Scripts/Testing/TEST_StackingDiamonds.cs
+++ Assets/YOUR_STUFF_HERE/Scripts/Testing/TEST_StackingDiamonds.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,34 +9,50 @@ public class TEST_StackingDiamonds : MonoBehaviour
 {
     [SerializeField] GameObject slotPrefab;
 
-    [SerializeField] GameObject[] grids;
-
-    GameObject[,] slotArray;
+    [SerializeField] GameObject[] uiGrids;
 
     [SerializeField] int maxRoundsTest;
-   
-    int roundsLeft = -1;
+    
+    int curRound = -1;
 
-    [SerializeField] float xPosOffset = 0;
-    [SerializeField] float yPosOffset = 0;
+    public float GridPosOffsetX = 48;
+    public float GridPosOffsetY = 64;
 
     [Tooltip("The horizontal positioning for the diagonal offset style every 2 rows")]
-    [SerializeField] float moduloOffset = 0;
+    public float GridModuloOffset = 68;
 
+    GameObject[,] gridOne;
+    GameObject[,] gridTwo;
+    GameObject[,] gridThree;
+    GameObject[,] gridFour;
+
+    int playersLeft = 4;
 
     // Start is called before the first frame update
     void Start()
     {
-        roundsLeft = maxRoundsTest-1;
+        curRound = 0;
 
-        for (int i = 0; i < grids.Length; i++)    
-            MakeSlots(i);
+        for (int i = 0; i < uiGrids.Length; i++)
+        {
+            GameObject[,] newGrid = MakeSlots(i);
+
+            switch(i)
+            {
+                case 0: gridOne = newGrid; break;
+                case 1: gridTwo = newGrid; break;
+                case 2: gridThree = newGrid; break;
+                case 3: gridFour = newGrid; break;
+            }
+        }
+
+        curGrid = gridOne;
     }
 
-    void MakeSlots(int playerIndex)
+    GameObject[,] MakeSlots(int playerIndex)
     {
-        slotArray = new GameObject[maxRoundsTest, 4];
-        Vector3 pos = grids[playerIndex].transform.position;
+        GameObject[,] slotArray = new GameObject[maxRoundsTest, 4];
+        Vector3 pos = uiGrids[playerIndex].transform.position;
 
         //Create rows for each round
         for (int m = 0; m < slotArray.GetLength(0); m++)
@@ -42,52 +60,35 @@ public class TEST_StackingDiamonds : MonoBehaviour
             //Create the panels on that row
             for (int p = 0; p < slotArray.GetLength(1); p++)
             {
-                var panel = Instantiate(slotPrefab, grids[playerIndex].transform);
-                panel.transform.position = new Vector3(pos.x-92 + (p * xPosOffset), pos.y-128 + (m * yPosOffset), 0);
+                var panel = Instantiate(slotPrefab, uiGrids[playerIndex].transform);
+                panel.transform.position = new Vector3(pos.x-92 + (p * GridPosOffsetX), pos.y-128 + (m * GridPosOffsetY), 0);
                 
                 if (m % 2 == 0)
-                    panel.transform.localPosition += Vector3.right * moduloOffset;
+                    panel.transform.localPosition += Vector3.right * GridModuloOffset;
+
+                panel.GetComponent<ChoiceSlot>().RowIndex = p;
 
                 slotArray[m, p] = panel;
             }
         }
+
+        return slotArray;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (roundsLeft < 0) return;
+        if (curRound > maxRoundsTest) return;
 
-        DeterminePlayerInput();
         DetermineInput();
 
         UpdateSlot();
-        
+
         if (Input.GetKeyDown(KeyCode.Space))
-            ChangeSlot();
+            AssignSlot();
     }
 
     int choice = -1;
-    int lastPlayer = -1;
-
-    int playerOneChoice = -1;
-    int playerTwoChoice = -1;
-
-    bool playerOneLocked = false;
-    bool playerTwolocked = false;
-
-    //TESTING ONLY
-    //Gets the last player who inputted
-    void DeterminePlayerInput()
-    {
-        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.A) ||
-        Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.D))
-            lastPlayer = 0;
-
-        if (Input.GetKeyDown(KeyCode.Y) || Input.GetKeyDown(KeyCode.G) ||
-        Input.GetKeyDown(KeyCode.H) || Input.GetKeyDown(KeyCode.J))
-            lastPlayer = 1;
-    }
 
     void DetermineInput()
     {
@@ -103,48 +104,137 @@ public class TEST_StackingDiamonds : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.H))
             choice = 3;
-
-        if(lastPlayer != -1)
-        {
-            switch(lastPlayer)
-            {
-                case 0: playerOneChoice = choice; break;
-                case 1: playerTwoChoice = choice; break;
-            }
-        }
     }
 
-    GameObject lastSelected = null;
+    ChoiceSlot lastSelected = null;
 
     void UpdateSlot()
     {
         if (choice == -1) return;
 
-        GameObject slot = slotArray[roundsLeft, choice];
+        ChoiceSlot slot = curGrid[curRound, choice].GetComponent<ChoiceSlot>();
 
         if (lastSelected != slot)
         {
             if(lastSelected != null)
                 lastSelected.GetComponent<Image>().color = Color.grey;
 
-            slot.GetComponent<ChoiceSlot>().OnSelected(choice);
+            slot.OnSelected(grid);
             
-            //slot.GetComponent<Image>().color = Color.blue;
-
             lastSelected = slot;
         }
     }
 
-    void ChangeSlot()
+    int grid = 0;
+    GameObject[,] curGrid;
+
+    void IncrementGridSelect()
     {
-        GameObject slot = slotArray[roundsLeft, choice];
-        slot.GetComponent<Image>().color = Color.green;
+        grid++;
+
+        if (grid > 3)
+            grid = 0;
+
+        if (lastSelected != null)
+            lastSelected.GetComponent<Image>().color = Color.grey;
 
         lastSelected = null;
 
-        playerOneChoice = -1;
-        playerTwoChoice = -1;
+        switch (grid)
+        {
+            case 0: curGrid = gridOne; break;
+            case 1: curGrid = gridTwo; break;
+            case 2: curGrid = gridThree; break;
+            case 3: curGrid = gridFour; break;
+        }
+    }
 
-        roundsLeft--;
+    void AssignSlot()
+    {
+        ChoiceSlot slot = curGrid[curRound, choice].GetComponent<ChoiceSlot>();
+        slot.Assign(grid);
+
+        lastSelected = null;
+
+        choice = -1;
+        playersLeft--;
+
+        if (playersLeft <= 0)
+            EndRoundResults();
+        else
+            IncrementGridSelect();
+    }
+
+    GameObject[,] GetGrid(int grid)
+    {
+        switch (grid)
+        {
+            case 0: return gridOne;
+            case 1: return gridTwo;
+            case 2: return gridThree;
+            case 3: return gridFour;
+
+            default: return null;
+        }
+    }
+
+    void EndRoundResults()
+    {
+        IncrementGridSelect();
+
+        List<ChoiceSlot> slots = GetSelectedFromGrids();
+
+        DetermineScore(slots);
+
+        playersLeft = 4;
+        curRound++;
+    }
+
+    List<ChoiceSlot> GetSelectedFromGrids()
+    {
+        List<ChoiceSlot> slots = new List<ChoiceSlot>();
+
+        for (int i = 0; i < uiGrids.Length; i++)
+        {
+            GameObject[,] grid = GetGrid(i);
+
+            for (int p = 0; p < 4; p++)
+            {
+                ChoiceSlot slot = grid[curRound, p].GetComponent<ChoiceSlot>();
+
+                if(slot.IsSelected)
+                    slots.Add(slot);
+            }
+        }
+
+        return slots;
+    }
+
+    int CountMatchingSlotOwners(ChoiceSlot curSlot, List<ChoiceSlot> slots)
+    {
+        //Counts how many players have selected that slot from row index
+        //EXAMPLE: if 2 players selected row 3, it'll return 2 otherwise 1
+        int owners = slots.Where(s => s.RowIndex == curSlot.RowIndex).Count(c => c.IsSelected);
+
+        return owners;
+    }
+
+    //Determine score from that round for each player
+    void DetermineScore(List<ChoiceSlot> slots)
+    {
+        foreach (ChoiceSlot slot in slots)
+        {
+            int totalOwners = CountMatchingSlotOwners(slot, slots);
+
+            //Multiple players has that slot
+            if (totalOwners > 1)
+                slot.GetComponent<Image>().color = Color.red;
+            //Only one player has that slot
+            else if (totalOwners == 1)
+            {
+                int player = slot.PlayerOwner;
+                slot.GetComponent<Image>().color = slot.GetSelectingPlayerColour(player);
+            }
+        }
     }
 }
